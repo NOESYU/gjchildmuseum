22a6851b64fb09b80f91e6f800fd02dcf9e87e4e
Jeon. <86655940+NOESYU@users.noreply.github.com>
2024-10-02 11:51:25 +0900

    모바일 메뉴, 메인 애니메이션

--- js/script.js
+++ js/script.js
@@ -7,3 +7,24 @@ $(".menu>li").mouseout(function(){
   $(this).children(".submenu").stop().hide();
   $(".header").removeClass("hover");
 })
+
+
+$(".m_submenu").hide();
+$(".m_menu>li").click(function(){
+  $(this).children(".m_submenu").slideToggle(200);
+  $(this).siblings().children(".m_submenu").slideUp(100);
+  $(this).toggleClass("m_active");
+  $(this).siblings().removeClass("m_active");
+})
+
+
+$(".m_nav").hide();
+$(".m_btn").click(function(){
+  $(".m_nav").fadeIn();
+  $(".m_nav_wrap").addClass("m_nav_active");
+})
+$(".m_close").click(function(){
+  $(".m_nav").fadeOut();
+  $(".m_nav_wrap").removeClass("m_nav_active");
+})
+
